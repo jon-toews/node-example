0dd21cbbaafbace83d9394ad54466117260c8b59
Jon Toews <jont974@gmail.com>
2017-11-11 01:39:55 -0600

    first commit

---
+++ public/javascripts/modules/map.js
@@ -0,0 +1,20 @@
+import axios from 'axios';
+
+const mapOptions = {
+  center: { lat: 43.2, lng: -79.8 },
+  zoom: 2,
+};
+
+
+function loadPlaces(map, lat = 43.2, lng = -79.8) {
+
+}
+
+function makeMap(mapDiv) {
+  if (!mapDiv) return;
+  // make our map
+  const map = new google.maps.Map(mapDiv, mapOptions);
+
+};
+
+export default makeMap;
